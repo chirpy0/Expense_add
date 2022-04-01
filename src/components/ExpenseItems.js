45556a516b4ef6e214a95c2e9454e4a4f07f0b02
ava_smith <chirpy0917@gmail.com>
2022-04-01 16:42:04 +0530

    folder arrangement

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,22 +1,22 @@
-// import React,{useState}  from 'react';
+import React,{useState}  from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css';
-import Card from './Card';
+import Card from './UI/Card';
 // import react from 'react';
 function ExpenseItem(props) {
-  // const [title,newTitle]=useState(props.title);
-  // function titleHandler(){
-  //   newTitle('updated');
-  //   console.log(title);
-  // }
+  const [title,newTitle]=useState(props.title);
+  function titleHandler(){
+    newTitle('updated');
+    console.log(title);
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      {/* <button onClick={titleHandler}>click</button> */}
+      <button onClick={titleHandler}>click</button>
     </Card>
   );
 }
