e359b07fbc1815e2b4e24bb52da2bdb0a4608227
ava smith <chirpy0917@gmail.com>
2022-04-05 00:19:14 +0530

    section-4 multi states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,28 +5,61 @@ const ExpenseForm=()=>{
     const [enteredTitle,setEnteredTitle]= useState('');
     const [enteredAmount,setEnteredAmount]= useState('');
     const [enteredDate,setEnteredDate]= useState('');
+    // const [userInput,setUserInput]= useState({
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     enteredDate:''
+    // });
     const titleChangehandler=(event)=>{
-        setEnteredTitle(event.target.title)
+        setEnteredTitle(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle:event.target.value,
+        // });
+        //this is better approach because it keeps updates snapshot
+        // setUserInput((previnput)=>{
+        //     return {...userInput,enteredTitle:event.target.value}        
+        // });
     }
     const amountChangehandler=(event)=>{
-        setEnteredAmount(event.target.title)
+        setEnteredAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:event.target.value,
+        // });
     }
     const dateChangehandler=(event)=>{
-        setEnteredDate(event.target.title)
+        setEnteredDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate:event.target.value,
+        // });
     }
-return <form>
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        const expenseData={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date: new Date(enteredDate),
+        }
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
+return <form onSubmit={submitHandler}>
     <div className='new-expense__controls'>
         <div className='new-expense__control'>
             <label>Title</label>
-            <input type='text' onChange={ titleChangehandler } />
+            <input type='text' value={ enteredTitle } onChange={ titleChangehandler } />
         </div>
         <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' min='0.01' step='0.01' onChange={ amountChangehandler } />
+            <input type='number' min='0.01' step='0.01' value={ enteredAmount } onChange={ amountChangehandler } />
         </div>
         <div className='new-expense__control'>
             <label>Title</label>
-            <input type='date' onChange={ dateChangehandler }/>
+            <input type='date' value={ enteredDate } onChange={ dateChangehandler }/>
         </div>
     </div>
     <div className='new-expense__actions'>
